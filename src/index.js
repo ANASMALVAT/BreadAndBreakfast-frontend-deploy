a1e5bf5e2790104c998979f1272a3ea7010844be
ANASMALVAT <an890779@dal.ca>
2024-01-07 00:59:03 -0400

    deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
-import './index.css';
-import App from './App';
-import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-Kommunicate.init("303f94dbb5cf7272b3ca7cf04105a077d" ,{"popupWidget":true,"automaticChatOpenOnNavigation":true});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
-
